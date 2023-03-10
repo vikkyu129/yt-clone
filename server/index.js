5733b46ce59e511c5cb5893fd43d1993cafcfead
Udaya Kudulla <udaya.kudulla@niruthi.com>
2023-03-10 22:39:52 +0530

    added routes and controllers templates

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-
+import videoRoutes from "routes/video";
+import commentRoutes from "routes/comment";
+import userRoutes from "routes/user";
 const app = express();
 const PORT = 8800;
 dotenv.config();
@@ -18,6 +20,11 @@ const connect = () => {
       );
     });
 };
+
+// routes
+app.use("/api/users", userRoutes);
+app.use("/api/videos", videoRoutes);
+app.use("/api/comments", commentRoutes);
 
 app.listen(8800, () => {
   console.log(`Listening on ${PORT}...`);
---
+++ server/routes/user.js
@@ -0,0 +1,5 @@
+import express from "express";
+import {} from "../controllers/UserController";
+const router = express.Router();
+
+export default router;
