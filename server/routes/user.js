7e5a9b55918af0203ec42e9aa17b426a7b3d7303
Udaya Kudulla <udaya.kudulla@niruthi.com>
2023-03-11 00:43:59 +0530

    new files

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,32 @@
 import express from "express";
-import {} from "../controllers/UserController";
+import {
+  update,
+  deleteUser,
+  getUser,
+  subscribe,
+  unsubscribe,
+  like,
+  dislike,
+} from "../controllers/UserController";
 const router = express.Router();
+//update user
+router.put("/:id", update);
 
+//delete user
+router.delete("/:id", deleteUser);
+
+//get a user
+router.get("/find/:id", getUser);
+
+//subscribe a user
+router.put("/sub/:id", subscribe);
+
+//unsubscribe a user
+router.put("/unsub/:id", unsubscribe);
+
+//like a video
+router.put("/like/:videoId", like);
+
+//dislike a video
+router.put("/dislike/:videoId", dislike);
 export default router;
